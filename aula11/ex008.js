3a18e6649c88bef78b9d1861c6de9a9218d7a7ee
ThiagoBenz <thiagobenz99@gmail.com>
2022-09-19 16:10:23 -0300

    inclusao da aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+//console.log(`funcionou corretamente`);
+ var vel  = 78.2;
+ console.log(`sua velocidade é de ${vel}km/h`)
+ if (vel >70) {
+    console.log(`você foi multado, limite de 70km/h`);
+ } 
+console.log(`Se beber não dirija!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var nacionalidade = 'EUA';
+if (nacionalidade == 'Brasil') {
+    console.log(`Você é brasileiro.`)
+} else {
+    console.log(`Você é estrangeiro.`)
+}
